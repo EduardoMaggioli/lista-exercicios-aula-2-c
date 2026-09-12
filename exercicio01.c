180e70f93c11a69dac2ad331063bfea379d7e7fc
Eduardo Maggioli dos Santos <141187128+EduardoMaggioli@users.noreply.github.com>
2026-09-12 19:22:02 -0300

    exercício 1

--- exercicio01.c
+++ exercicio01.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main()
+{
+    int numeros[10];
+    int i;
+
+    printf("Digite 10 numeros inteiros:\n");
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("Numero %d: ", i + 1);
+        scanf("%d", &numeros[i]);
+    }
+
+    printf("\nNumeros na ordem digitada:\n");
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d ", numeros[i]);
+    }
+
+    printf("\n\nNumeros na ordem inversa:\n");
+
+    for (i = 9; i >= 0; i--)
+    {
+        printf("%d ", numeros[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
