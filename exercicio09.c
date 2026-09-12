171da54a6f05f2737998564ab016068b4f6b4061
Eduardo Maggioli dos Santos <141187128+EduardoMaggioli@users.noreply.github.com>
2026-09-12 19:32:25 -0300

    exercicio09.c

---
+++ exercicio09.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+#define MAX_ALUNOS 30
+
+void lerNotas(float notas[], int quantidade)
+{
+    int i;
+
+    for (i = 0; i < quantidade; i++)
+    {
+        printf("Digite a nota do aluno %d: ", i + 1);
+        scanf("%f", &notas[i]);
+    }
+}
+
+float calcularMedia(float notas[], int quantidade)
+{
+    float soma = 0;
+    int i;
+
+    for (i = 0; i < quantidade; i++)
+    {
+        soma = soma + notas[i];
+    }
+
+    return soma / quantidade;
+}
+
+float encontrarMaiorNota(float notas[], int quantidade)
+{
+    float maior = notas[0];
+    int i;
+
+    for (i = 1; i < quantidade; i++)
+    {
+        if (notas[i] > maior)
+        {
+            maior = notas[i];
+        }
+    }
+
+    return maior;
+}
+
+float encontrarMenorNota(float notas[], int quantidade)
+{
+    float menor = notas[0];
+    int i;
+
+    for (i = 1; i < quantidade; i++)
+    {
+        if (notas[i] < menor)
+        {
+            menor = notas[i];
+        }
+    }
+
+    return menor;
+}
+
+void imprimirRelatorio(float notas[], int quantidade)
+{
+    int i;
+
+    printf("\n----- RELATORIO DA TURMA -----\n");
+
+    for (i = 0; i < quantidade; i++)
+    {
+        printf("Aluno %d: %.2f\n", i + 1, notas[i]);
+    }
+
+    printf("\nMedia da turma: %.2f\n", calcularMedia(notas, quantidade));
+    printf("Maior nota: %.2f\n", encontrarMaiorNota(notas, quantidade));
+    printf("Menor nota: %.2f\n", encontrarMenorNota(notas, quantidade));
+}
+
+int main()
+{
+    float notas[MAX_ALUNOS];
+    int quantidade;
+
+    printf("Digite a quantidade de alunos: ");
+    scanf("%d", &quantidade);
+
+    lerNotas(notas, quantidade);
+    imprimirRelatorio(notas, quantidade);
+
+    return 0;
+}
